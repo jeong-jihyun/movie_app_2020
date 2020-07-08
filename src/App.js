4518e92b93e8d19d9917b5e0cefc191aec90317e
jeongjihyun <mujuk73@hotmail.com>
2020-07-09 00:20:35 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -8,17 +8,68 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Detail from './routes/Detail';
 import Todo from './routes/Todo';
+import { createStore } from 'redux';
+const initialState = {
+  tasks: []
+};
+
+function addReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task])
+      };
+    default:
+      return state;
+  }
+}
+
+function resetReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'RESET_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([])
+      };
+    default:
+      return state;
+  }
+}
+
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+
+const store = createStore(addReducer);
+store.dispatch(addTask('스토어 Study'));
+console.log(store.getState());
+
+store.replaceReducer(resetReducer);
+console.log(store.getState());
+
+const resetTask = () => ({
+  type : 'RESET_TASK'
+});
+
+store.dispatch(resetTask());
+
+store.dispatch(addTask('리듀서 Study'));
+console.log(store.getState());
+
 
 class App extends React.Component {
-
   render() {
     return (
       <HashRouter>
-        <Navigation/>
+        <Navigation />
         <Route path="/" exact={true} component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/movie-detail" component={Detail}/>
-        <Route path="/todo" component={Todo}/>
+        <Route path="/movie-detail" component={Detail} />
+        <Route path="/todo" component={Todo} />
       </HashRouter>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
 
+const store = createStore(tasksReducer);
+const initialState = {
+  tasks: []
+};
+const inputTask = (task) => ({
+  type: 'INPUT_TASK',
+  payload: {
+    task
+  }
+});
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'INPUT_TASK':
+      return {
+        ...state,
+        tasks: state.payload.task
+      };
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task])
+      };
+    default:
+      return state;
+  }
+}
+
+function handleChange() {
+  console.log(store.getState());
+}
+
+// eslint-disable-next-line no-unused-vars
+const unsubscribe = store.subscribe(handleChange);
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+store.dispatch(addTask('스토어 Study'));
+console.log(store.getState());
 
 ReactDOM.render(
   <App />,
